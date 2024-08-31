76ab0c31a593a9ea545dd92f42d4c8250d0e1ec5
Korozco1 <karl.orozcochinchilla@wu.com>
2024-08-31 17:29:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,13 @@ if ingredients_list:
       session.sql(my_insert_stmt).collect()
     
       st.success('Your Smoothie is ordered, ' + Name_on_order + "!",  icon="✅")
+     #st.stop()
+
  # New section to deisplay fruityvice nutrition information           
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())        
-    #st.stop()
+#st.text(fruityvice_response.json())        
+fv_df =st.dataframe(data=fruityvice_response.jaon(), use_container_width=true)
+
+
+
